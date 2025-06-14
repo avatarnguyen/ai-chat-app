794a07aa0e6efc4e9eb69639ed0471d2fec0f5f2
Anh Fimo <anh@fimo.io>
2025-06-14 17:27:19 +0400

    Added Flavors to the project.

---
+++ lib/main_prod.dart
@@ -0,0 +1,11 @@
+import 'package:ai_chat_app/flavors.dart';
+import 'main.dart';
+
+// * Entry point for the prod flavor
+void main() async {
+  // Add environnement dependent code here
+  // ...
+  
+  F.appFlavor = Flavor.prod;
+  await runMainApp();
+}
